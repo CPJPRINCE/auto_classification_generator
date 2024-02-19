5772a4140ba1a28ba1f27604ea72ce793d4bcc2b
CPJPRINCE <c.pj.prince@gmail.com>
2024-02-19 14:17:14 +0000

    packaging finalisation

--- auto_classification_generator/__init__.py
+++ auto_classification_generator/__init__.py
@@ -7,7 +7,8 @@ license: Apache License 2.0"
 
 from .common import *
 from .classification_generator import ClassificationGenerator
+from .cli import parse_args,run_cli
 
 __author__ = "Christopher Prince (c.pj.prince@gmail.com)"
-__version__ = "2.0.1"
+__version__ = "1.0.0"
 __license__ = "Apache License Version 2.0"
