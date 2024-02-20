19306f2ce39714527a1443b56da295cef7812553
CPJPRINCE <c.pj.prince@gmail.com>
2024-02-20 15:36:11 +0000

    Version updates and changes to cli

--- auto_classification_generator/__init__.py
+++ auto_classification_generator/__init__.py
@@ -8,7 +8,7 @@ license: Apache License 2.0"
 from .common import *
 from .classification_generator import ClassificationGenerator
 from .cli import parse_args,run_cli
-
+from .version import __version__
 __author__ = "Christopher Prince (c.pj.prince@gmail.com)"
-__version__ = "1.0.0"
+__version__ = __version__
 __license__ = "Apache License Version 2.0"
--- auto_classification_generator/cli.py
+++ auto_classification_generator/cli.py
@@ -1,10 +1,11 @@
 from auto_classification_generator.classification_generator import ClassificationGenerator
+from auto_classification_generator.version import __version__
 import argparse
 import os
 
 def parse_args():
     parser = argparse.ArgumentParser(description="OPEX Manifest Generator for Preservica Uploads")
-    parser.add_argument('rootpath',nargs='?', default=os.getcwd())
+    parser.add_argument('root',nargs='?', default=os.getcwd())
     parser.add_argument("-p","--prefix",required=False, nargs='?')
     parser.add_argument("-accp", "--acc-prefix",required=False, nargs='?')
     parser.add_argument("-rm","--empty",required=False,action='store_true')
@@ -14,21 +15,24 @@ def parse_args():
     parser.add_argument("-m","--meta-dir",required=False,action='store_true',default=True)
     parser.add_argument("--skip",required=False,action='store_true',default=False)
     parser.add_argument("-fmt","--output-format",required=False,default="xlsx",choices=['xlsx','csv'])
-    
+    parser.add_argument("-v", "--version", action='version',version='%(prog)s {version}'.format(version=__version__))
     args = parser.parse_args()
     return args
 
 def run_cli():
     args = parse_args()
-    if isinstance(args.rootpath,str): args.rootpath = args.rootpath.strip("\"").rstrip("\\")
+    if isinstance(args.root,str): args.root = args.root.strip("\"").rstrip("\\")
     if not args.output:
-        args.output = os.path.abspath(args.rootpath)
+        args.output = os.path.abspath(args.root)
         print(f'No output path selected, defaulting to root Directory: {args.output}')
     else:
         args.output = os.path.abspath(args.output)
         print(f'Output path set to: {args.output}')
-        
-    ClassificationGenerator(args.rootpath,
+    if args.version:
+        print(__version__)
+        raise SystemExit()
+    
+    ClassificationGenerator(args.root,
                             output_path=args.output,
                             prefix=args.prefix,
                             accprefix=args.acc_prefix,
